4a60b4579800520b4fa2add1ea256ce9d41fb186
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-01 19:37:37 +0700

    move import to env variable

--- lib/user.ex
+++ lib/user.ex
@@ -1,19 +1,20 @@
 defmodule Mehub.User do
-  import Mehub.GithubClient, only: [make_request: 2]
+
+  @github_client Application.get_env(:mehub, :github_client)
 
   def get_user(username) do
-    make_request(:get, "/users/#{username}")
+    @github_client.make_request(:get, "/users/#{username}")
   end
 
   def get_repos(username) do
-    make_request(:get, "/users/#{username}/repos")
+    @github_client.make_request(:get, "/users/#{username}/repos")
   end
 
   def get_orgs(username) do
-    make_request(:get, "/users/#{username}/orgs")
+    @github_client.make_request(:get, "/users/#{username}/orgs")
   end
 
   def get_followers(username) do
-    make_request(:get, "/users/#{username}/followers")
+    @github_client.make_request(:get, "/users/#{username}/followers")
   end
 end
