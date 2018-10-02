916f608e836b42f88171986c0dc3de7e0f5e3199
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-02 22:12:26 +0700

    user module using spec

--- lib/user.ex
+++ lib/user.ex
@@ -2,11 +2,15 @@ defmodule Mehub.User do
 
   @github_client Application.get_env(:mehub, :github_client)
 
+  @spec get_user(String.t) :: {:ok, HTTPoison.Response.t()}
   def get_user(username), do: @github_client.make_request(:get, "/users/#{username}")
 
+  @spec get_repos(String.t) :: {:ok, HTTPoison.Response.t()}
   def get_repos(username), do: @github_client.make_request(:get, "/users/#{username}/repos")
 
+  @spec get_orgs(String.t) :: {:ok, HTTPoison.Response.t()}
   def get_orgs(username), do: @github_client.make_request(:get, "/users/#{username}/orgs")
 
+  @spec get_followers(String.t) :: {:ok, HTTPoison.Response.t()}
   def get_followers(username), do: @github_client.make_request(:get, "/users/#{username}/followers")
 end
